43d41d23cae18be7d3305aa3805d3445948caf1c
Danielle Posthuma <pgcdac@int5.local.snellius.surf.nl>
2023-07-28 14:26:37 +0200

    added function that solve the symlinks also with replacement (--replace)

--- symlinks_snellius.py
+++ symlinks_snellius.py
@@ -2,11 +2,12 @@ import os
 import sys 
 import argparse
 from pathlib import Path
-__version__ = "2023_July.v.4.0"
+__version__ = "2023_July.v.5.0"
 parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
 parser.add_argument("--version", "-v", action="version", version=f"Version: {__version__}")
-parser.add_argument('--dir', default=None, type=str, help="FULL Path of the directory from where you want to start symlink check recursively\n", required=True)
+parser.add_argument('--dir', default=None, type=str, help="FULL PATH of the directory from where you want to start symlinks check recursively\n", required=True)
 parser.add_argument('--out', default=None, type=str, help="Mandatory outname\n", required=True)
+parser.add_argument('--replace', default=None, help="If you want to solve by replacement.\nThis takes comma seprated argument with directoy pattern that needs to be replaced (e.g. /dir1/dir2) and replacement( e.g. /dir3/dir4).\nIn the example /dir1/dir2 will be replaced by /dir3/dir4 and then target will be checked if exist or not.\nCan be used with --update-links to solve if targets  for which new link (after replacement) exists.")
 parser.add_argument('--update-links', action='store_true', help="If you want to update the symlinks on snellius (it will only update if the target data present)")
 
 cwd = os.getcwd()
@@ -22,7 +23,109 @@ def update_symlink(current_symlink, new_location):
     os.remove(current_symlink)
     os.symlink(new_location, current_symlink)
 
-def find_symlinks(directory, out, update_links):
+def find_and_replace(old_target, old_dir_pat, new_dir_pat):
+    if old_dir_pat in old_target:
+        new_target = old_target.replace(old_dir_pat, new_dir_pat)
+    else:
+        new_target = old_target
+    return new_target
+
+def find_symlinks_ge(directory, out, update_links, replace):
+    if os.path.exists(f'symlinks.{out}.summary.logs'):
+        print(f"symlinks.{out}.summary.logs file already Exists!\nPlease change --out and re-run the script.")
+        sys.exit()
+    else:
+        logger_object = Logger(f'symlinks.{out}.summary.logs')
+        header="##Warnigs\tLocation\tTarget_location\tNewTarget_location(if_found)"
+        logger_object.log(header)
+    counters = {"W0": 0, "W1": 0, "W2": 0, "W3": 0, "W4": 0, "W5": 0, "W6": 0,"U1": 0, "U5": 0}
+    for root, dirs, files in os.walk(directory): ## recursive
+        for name in files + dirs:
+            path = os.path.join(root, name)
+            if os.path.islink(path):
+                target = os.readlink(path)
+                components = target.split('/')
+                components = [comp for comp in components if comp]
+                full_link= False if any(('.' == item) | ('..' == item) for item in components) else True
+                
+                if len(components)<2:
+                    line = f"Warning00:link_other_unexpected:\t{path}\t{target}\t{target}"
+                    logger_object = Logger(f'symlinks.{out}.summary.logs')
+                    logger_object.log(line)
+                    counters["W0"] += 1
+                    continue
+                if (os.path.isabs(target)) & (full_link):   ## symlinks with full pat
+                    symlink_path = path; target_path = target
+                    old_dir_pat=""; new_dir_pat=""
+                    if len(replace.split(","))==2:
+                        old_dir_pat, new_dir_pat=replace.split(",")
+                    else:
+                        print("Please check the argument in --replace. It should be comma seperated and 2 paths")
+                        exit()
+                
+                    new_target_path=find_and_replace(target_path, old_dir_pat, new_dir_pat) 
+                    if target_path==new_target_path:
+                        if os.path.exists(target_path):
+                            line = f"Warning02:link_exist_without_rep\t{symlink_path}\t{target_path}\t{new_target_path}"
+                            counters["W2"] += 1
+                        else:
+                            line = f"Warning03:link_nonexist_after_rep\t{symlink_path}\t{target_path}\t{new_target_path}"
+                            counters["W3"] += 1
+                    elif os.path.exists(new_target_path):
+                        line = f"Warning01:link_exist_after_rep\t{symlink_path}\t{target_path}\t{new_target_path}"
+                        counters["W1"] += 1
+                        if update_links:
+                            update_symlink(symlink_path, new_target_path)
+                            line = f"Updated01:rsynclink_pgcdac_solvable\t{symlink_path}\t{target_path}\t{new_target_path}"
+                            counters["U1"] += 1
+                    else:
+                        if os.path.exists(target_path):
+                            line = f"Warning02:link_exist_without_rep\t{symlink_path}\t{target_path}\t{new_target_path}"
+                            counters["W2"] += 1
+                        else:
+                            line = f"Warning03:link_nonexist_after_rep\t{symlink_path}\t{target_path}\t{new_target_path}"
+                            counters["W3"] += 1
+
+                else:## symlinks with relative path
+                    if   (components[0]=="rsyncd-munged") & (components[1]==".."):
+                        target_path='/'.join(components[1:])
+                    elif (components[0]=="rsyncd-munged") & (components[1]=="."):
+                        target_path='../../'+'/'.join(components[2:])
+                    else:
+                        target_path='/'.join(components[0:])
+                    
+                    os.chdir(root) 
+                    if os.path.exists(target):
+                        line = f"Warning04:rellink_other_existing\t{root}\t{target}\t{target_path}"
+                        counters["W4"] += 1
+                    elif os.path.exists(target_path):
+                        if update_links:
+                            update_symlink(path, target_path)
+                            line = f"Updated05:rellink_rysncd_solvable\t{root}\t{target}\t{target_path}"
+                            counters["U5"] += 1
+                        else:
+                            line = f"Warning05:rellink_rysncd_solvable\t\t{root}\t{target}\t{target_path}"
+                            counters["W5"] += 1
+                    else:
+                        line = f"Warning06:rellink_other_nonexisting\t{root}\t{target}\t{target_path}"
+                        counters["W6"] += 1
+                    os.chdir(cwd)
+
+                logger_object = Logger(f'symlinks.{out}.summary.logs')
+                logger_object.log(line)
+        
+    if update_links:
+        summs=f"##Warning00(link_other_unexpected):{counters['W0']}\n##Updated01(link_exist_after_rep):{counters['U1']}\n##Warning02(link_exist_without_rep):{counters['W2']} \
+              \n##Warning03(link_notexist_after_rep):{counters['W3']}\n##Warning04(rellink_other_existing):{counters['W4']}\n##Updated5(rellink_rysncd_solvable):{counters['U5']}\
+              \n##Warning06(rellink_other_nonexisting):{counters['W6']}"
+    else:
+        summs=f"##Warning00(link_other_unexpected):{counters['W0']}\n##Warning01(link_exist_after_rep):{counters['W1']}\n##Warning02(link_exist_without_rep):{counters['W2']} \
+              \n##Warning03(link_notexist_after_rep):{counters['W3']}\n##Warning04(rellink_other_existing):{counters['W4']}\n##Warning05(rellink_rysncd_solvable):{counters['W5']} \
+              \n##Warning06(rellink_other_nonexisting):{counters['W6']}"
+    logger_object = Logger(f'symlinks.{out}.summary.logs')
+    logger_object.log(summs)
+
+def find_symlinks_pd(directory, out, update_links):
     if os.path.exists(f'symlinks.{out}.summary.logs'):
         print(f"symlinks.{out}.summary.logs file already Exists!\nPlease change --out and re-run the script.")
         sys.exit()
@@ -94,6 +197,12 @@ def find_symlinks(directory, out, update_links):
                             counters["W7"] += 1
                 
                 else:## symlinks with relative path
+                    if len(components)<2:
+                        line = f"Warning0:symlink_other_unexpected:\t{path}\t{target}\t{target}"
+                        logger_object = Logger(f'symlinks.{out}.summary.logs')
+                        logger_object.log(line)
+                        counters["W0"] += 1
+                        continue
                     if   (components[0]=="rsyncd-munged") & (components[1]==".."):
                         target_path='/'.join(components[1:])
                     elif (components[0]=="rsyncd-munged") & (components[1]=="."):
@@ -136,6 +245,9 @@ def find_symlinks(directory, out, update_links):
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    path = Path(args.dir); out=args.out; update_links = args.update_links
-    find_symlinks(path, out, update_links)
+    path = Path(args.dir); out=args.out; update_links = args.update_links; replace=args.replace
+    if replace:
+        find_symlinks_ge(path, out, update_links, replace)
+    else:
+        find_symlinks_pd(path, out, update_links)
 
